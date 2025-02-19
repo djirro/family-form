89395bf81735a364353e39b2682b477a65e7a79e
Egor Danshov <djirrochannel@gmail.com>
2025-02-19 14:18:43 +0300

    Создание пустого проекта

---
+++ FamilyForm/FamilyForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamilyForm
+//
+//  Created by Егор Даншов on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
